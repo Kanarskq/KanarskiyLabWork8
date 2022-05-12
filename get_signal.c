e6319c5050cbb827cd2bee3cb4a0d6439f16cbd2
Kanarskq <102312997+Kanarskq@users.noreply.github.com>
2022-05-13 00:56:07 +0300

    Add files via upload

--- get_signal.c
+++ get_signal.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>	
 
-static void sig_usr(int signo){
-        if (signo == SIGUSR2)
-               printf("«Process of Kanarskiy got signal»\n");		
+static void sig_user(int signO){
+        if (signO == SIGUSR2)
+               printf("«Process of Kanarkiy got signal»\n");		
 }
 int main(void){
-        if (signal(SIGUSR2, sig_usr) == SIG_ERR)
+        if (signal(SIGUSR2, sig_user) == SIG_ERR)
                 fprintf(stderr, "Hеможливо перехопити сигнал SIGUSR2\n");
         printf("Мій PID=%d.Чекаю на сигнал SIGUSR2\n",getpid());
         for ( ; ; )
