1537cd89084f6e20b1d63fed9c0d232d0c1dad79
Kanarskq <102312997+Kanarskq@users.noreply.github.com>
2022-05-13 00:38:12 +0300

    Add files via upload

---
+++ cat_Kanarskiy .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+extern char ** environ;
+int main (void)
+{
+        char* cat_args []={   "cat","/etc/hosts",NULL };
+        pid_t pid=fork();
+        if (pid == 0){
+                printf("\n The child of Kanarskiy print next info:\n");
+                execve ("/bin/cat", cat_args, environ);
+                printf("execve failed\n");
+                return EXIT_FAILURE;
+		}
+		
+return EXIT_SUCCESS;
+}
